f31d2a5c3ed475080e24baf3063c6fa7b0f8f167
toh12345 <44148216+toh12345@users.noreply.github.com>
2018-12-24 14:49:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/lab3_2resources/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.taruc.lab3_2resources;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMessage;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewMessage = findViewById(R.id.textViewMessage);
+
+    }
+
+    public void showMessgae (View view){
+
+        textViewMessage.setText(getString(R.string.message));
+
+    }
+
+}
